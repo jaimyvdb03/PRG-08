100594bf27eae8ade93a36f09d694dd0425f985e
jaimyvdb03 <jaimyvdb03@gmail.com>
2025-04-12 15:10:18 +0200

    base change

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'PRG-08',  // <- VERVANG dit!
+  base: '/PRG-08/',
   build: {
     outDir: 'docs'
   },
