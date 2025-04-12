7ab689d29e27c9badd72f399aacf80b2baa5261f
jaimyvdb03 <jaimyvdb03@gmail.com>
2025-04-12 15:05:33 +0200

    pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: 'PRG-08',  // <- VERVANG dit!
+  build: {
+    outDir: 'docs'
+  },
   plugins: [react()],
 })
